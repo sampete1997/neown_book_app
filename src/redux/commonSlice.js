a1fafb6745de496fa420badb825a0bf73ae4bdde
sohelpatel <sohel.p@eccentricengine.com>
2024-11-18 11:17:06 +0530

    udpated loader and env

--- src/redux/commonSlice.js
+++ src/redux/commonSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const commonSlice = createSlice({
   name: 'common',
   initialState: {
-    isLoading: true,
+    isLoading: false,
     toast: {
         isError: false,
         message: ""
